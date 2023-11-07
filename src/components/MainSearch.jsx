5807c80fe07b8b2852e9eceefff850d5c63bdbab
federicopignatelli <federicopignatelli@icloud.com>
2023-11-07 15:51:06 +0100

    buttonnotused-fixed

--- src/components/MainSearch.jsx
+++ src/components/MainSearch.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { Container, Row, Col, Form, } from "react-bootstrap";
 import Job from "./Job";
 import { Link } from "react-router-dom";
 
