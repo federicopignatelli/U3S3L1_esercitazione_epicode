1fdff1bdb347ca9db482a6047d02f2292c0bbd0d
federicopignatelli <federicopignatelli@icloud.com>
2023-11-07 15:22:35 +0100

    first commit

---
+++ src/components/Job.jsx
@@ -0,0 +1,33 @@
+import { Row, Col, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+
+const Job = ({ data }) => {
+  const dispatch = useDispatch()
+
+  return (
+    <Row
+      className="mx-0 mt-3 p-3"
+      style={{ border: '1px solid #00000033', borderRadius: 4 }}
+    >
+      <Col xs={3}>
+        <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+      </Col>
+      <Col xs={9}>
+        <a href={data.url} target="_blank" rel="noreferrer">
+          {data.title}
+        </a>
+      </Col>
+      <Col xs={3}>
+        <Button variant='info'
+          onClick={() => {
+            dispatch({
+              type: 'ADD_TO_FAVOURITES',
+              payload: data.company_name
+            })
+          }}>Aggiungi ai preferito</Button>
+      </Col>
+    </Row>
+  )
+}
+export default Job
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,24 @@
+const initialState = {
+    favourites: {
+        content: []
+    }
+}
+
+
+const mainReducers = (state = initialState, action) => {
+    switch (action.type) {
+        case 'ADD_TO_FAVOURITES':
+            return {
+                ...state,
+                favourites: {
+                    ...state.favourites,
+                    content: [...state.favourites.content, action.payload],
+                }
+            }
+
+        default:
+            return state
+    }
+}
+
+export default mainReducers
